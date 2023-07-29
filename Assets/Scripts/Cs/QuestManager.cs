4029838ec997d6676b792d7aeae824e9602c956d
mrhelgpro <mrhelgpro@gmail.com>
2023-07-29 16:27:56 +0300

    Update

--- Assets/Scripts/Cs/QuestManager.cs
+++ Assets/Scripts/Cs/QuestManager.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-
 using UnityEngine;
 using Naninovel;
 using TMPro;
@@ -11,11 +7,6 @@ public class QuestManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _textMeshProText; 
     private ICustomVariableManager _variableManager;
     private CustomVariableTrigger _customVariableTrigger;
-
-    public RectTransform Marker;
-    public RectTransform Location1;
-    public RectTransform Location2;
-    public RectTransform Location3;
 
     private void Start()
     {
